762258eff9ebf14d3125df574f87360c922613cb
ganesh <ganeshprabh07@gmail.com>
2020-04-05 17:59:19 +0400

    Change at feature branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React on feature branch
+          learn react on feature branch
         </a>
       </header>
     </div>
